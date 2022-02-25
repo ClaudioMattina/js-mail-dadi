11a91f16cdec8f95319a649eb5cc47ea49074deb
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-02-25 15:52:53 +0100

    tasks secondo esercizio

--- script.js
+++ script.js
@@ -7,10 +7,10 @@
 const emailUtente = prompt("inserisci emali");
 const emailNecessarie = [ "ciccio.pasticcio@ciccio.com", "ci.si.becca@ciccio.com", "bella.raga@ciccio.com", "okkeeeeeiiii@ciccio.com"];
 
-console.log(emailNecessarie)
-
+/* ho creato un ciclo for che dice: i = 0, i deve essere minore uguale della lunghezza dell'array (del numero di elementi presenti nell'array), fai un'altro giro nel caso sia si*/
 for(i = 0; i<= emailNecessarie.length; i++){
 
+    /* usando includes gli ho detto: se nell'insieme (array) delle emeailNecessarie è incluso l'input dell'utente scrivi bravo altrimenti scrivi cattivo */
     if(emailNecessarie.includes(emailUtente)) {
         console.log("bravo");
     }
@@ -18,3 +18,12 @@ for(i = 0; i<= emailNecessarie.length; i++){
         console.log("cattivo")
     };
 };
+
+
+
+/* SECONDO ESERCIZIO */
+
+/* generiamo un numero casuale da 1 a 6 dato dal giocatore*/
+/* generiamo un numero casuale da 1 a 6 dato dal PC */
+/* se il pc fa genera il numero più grande stampare "IA vince" se è il giocatore a fare il n più grande, stampare "Human vince" */
+/* nel caso di pareggio stampare "ritiriamo" */
