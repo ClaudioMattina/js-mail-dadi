3f66ff2b2e05099d935cbbb28946152880765601
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-02-25 15:06:28 +0100

    array

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+/* creare array con le email che possono accedere */
 /* richiedere l'imput dell'email dall'utente */
 /* è nella lista di quelli che possono accedere ? */
 /* se si, scrivi un messaggio appropriato, se no, scrivi un messaggio appropriato alla risposta negativa */
+
+const emailUtente = [ "ciccio.pasticcio@ciccio.com", "ci.si.becca@ciccio.com", "bella.raga@ciccio.com", "okkeeeeeiiii@ciccio.com"];
 
