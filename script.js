6855a88fd1ac9a0038aff639c4ed640f2ac10392
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-02-25 15:00:56 +0100

    inizio

---
+++ script.js
@@ -0,0 +1,4 @@
+/* richiedere l'imput dell'email dall'utente */
+/* è nella lista di quelli che possono accedere ? */
+/* se si, scrivi un messaggio appropriato, se no, scrivi un messaggio appropriato alla risposta negativa */
+
